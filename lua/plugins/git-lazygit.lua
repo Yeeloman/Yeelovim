96c492ce6e599713680e0ba6b727dd6a1dc43e56
yeeloman <kitten@TheJester.eldergods.org>
2023-11-16 05:17:19 +0100

    add lazygit

---
+++ lua/plugins/git-lazygit.lua
@@ -0,0 +1,7 @@
+-- check https://github.com/kdheepak/lazygit.nvim 
+-- for more informations
+return {
+  "kdheepak/lazygit.nvim",
+  lazy = false,
+  vim.keymap.set('n', '<leader>gg', ":LazyGit<CR>", { noremap = true, silent = true }),
+}
